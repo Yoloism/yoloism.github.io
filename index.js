27ca0572f7149045a9172a57055892724a4c56fc
Yoloism <jeremy_tay@hotmail.com>
2017-05-09 20:29:49 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// API EndPoint
+var url =  "http://api.icndb.com/jokes/random";
+
+// Get Ordered List Element
+var ol = document.getElementById("jokes");
+
+// Create Request
+var request = new XMLHttpRequest();
+request.open("GET", url, true);
+
+request.onload = function(){
+  // Handle Response
+	if(request.status == 200) { //
+	
+		var jsonObject = JSON.parse(request.response);
+		console.log(jsonObject.value.joke); // JSONObject
+		var li = document.createElement("li");
+		li.innerHTML = jsonObject.value.joke
+		ol.appendChild(li);
+	}
+}
+
+
+// Send Request
+request.send();
